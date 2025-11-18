817abe2f7680dca9bb45ccdbdd26c3499ef1e01a
Sara Alaidroos <sara.alaidroos@gmail.com>
2025-11-18 09:51:53 -0600

    added ArtifactID

--- src/main/java/ProjectThreeServer.java
+++ src/main/java/ProjectThreeServer.java
@@ -6,14 +6,13 @@ import javafx.application.Application;
 
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 
-public class JavaFXTemplate extends Application {
+public class ProjectThreeServer extends Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
